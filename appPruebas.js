0aef39598acd35516ababe40db067088d1be23d8
Nelson <jhoeladarmes@gmail.com>
2023-05-31 00:53:45 -0300

    calibrado de velocidad

--- appPruebas.js
+++ appPruebas.js
@@ -201,10 +201,10 @@ function repetirClicks(datos,inputAuxiliar,botonAux,timer,timer2){
         iterador2 = 0;
       }
       else{}
-      },20)
+      },50)
     
     }
-  , 250);
+  , 350);
 }
 
 function detenerRepetidorDeClicks(timer){
